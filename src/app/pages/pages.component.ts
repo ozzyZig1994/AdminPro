cd4262e5eed033fe8b251370645d072fbf19f6c2
ozzyZig1994 <adio_him@hotmail.com>
2022-05-21 09:03:52 -0500

    ng class

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -9,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class PagesComponent implements OnInit {
 
   year : Number = new Date ().getFullYear ();
+  public linkTheme = document.querySelector ('#theme');
 
   constructor() { }
 
   ngOnInit(): void {
+    const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
+
+    this.linkTheme?.setAttribute('href', url);
   }
 
 }
